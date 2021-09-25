719504c79fc654c5033792d5d5502991d30be300
monognuisy <ysmsmart1@gmail.com>
2021-09-25 20:36:43 +0900

    feat: add timer

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,25 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Timer = () => {
-  const [time, setTime] = useState(0);
+  const [second, setSecond] = useState(0);
 
   const onIncrease = () => {
-    setTime((n) => n + 1);
+    setSecond((n) => n + 10);
   };
 
   const onDecrease = () => {
-    setTime((n) => n - 1);
+    if (second < 10) {
+      setSecond(0);
+    } else {
+      setSecond((n) => n - 10);
+    }
   };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputTime: number = parseInt(e.target.value, 10);
+
+    if (Number.isNaN(inputTime)) {
+      setSecond(0);
+    } else {
+      setSecond(inputTime);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (second > 0) {
+        setSecond((s) => s - 1);
+      }
+    }, 1000);
+    return (() => clearInterval(interval));
+  }, [second]);
 
   return (
     <div className="timer">
       <div className="timer-text-box">
-        <h1>{time}</h1>
+        <h1>{second}</h1>
       </div>
       <div className="timer-button-box">
-        <button type="button" onClick={onIncrease}>+</button>
-        <button type="button" onClick={onDecrease}>-</button>
+        <button type="button" onClick={onIncrease}>+10</button>
+        <button type="button" onClick={onDecrease}>-10</button>
       </div>
+      <input placeholder="Write down Time(second)" onChange={handleChange} />
     </div>
   );
 };
