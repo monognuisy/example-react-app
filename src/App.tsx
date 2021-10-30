8a547f71d620594b2d3799c94bb66a67e7868747
monognuisy <ysmsmart1@gmail.com>
2021-10-30 16:34:08 +0900

    feat: google authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
+import type { User } from 'firebase/auth';
 import React from 'react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 import './style/App.css';
 import Timer from './components/Timer';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState<{
+    token: string | null;
+    user: User | null;
+    isLoggedIn: boolean;
+  }>({
+    token: null,
+    user: null,
+    isLoggedIn: !!user,
+  });
+
+  // App state에 token과 user, login여부를 설정한다.
+  const setAuth = ({ token, user }: { token: string | null, user: User | null}) => {
+    setState({
+      token,
+      user,
+      isLoggedIn: !!user,
+    });
+  };
+
   return (
     <div className="App">
-      <Timer />
+      {
+        state.isLoggedIn
+          ? (
+            <div>
+              <Timer />
+              <LogoutButton onLogout={() => setAuth({
+                token: null,
+                user: null,
+              })}
+              />
+            </div>
+          )
+          : <LoginButton onLogin={setAuth} />
+      }
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/googleAuthentication.ts
@@ -0,0 +1,35 @@
+import {
+  getAuth, signInWithPopup, signOut, GoogleAuthProvider,
+} from 'firebase/auth';
+
+// 구글 로그인
+export const googleLogin = async () => {
+  try {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    // 팝업창을 통해 구글 로그인 진행, 진행 후 결과 값 저장
+    const result = await signInWithPopup(auth, provider) ?? null;
+    const credential = GoogleAuthProvider.credentialFromResult(result) ?? null;
+    if (credential === null) {
+      console.error('failed to signin');
+      return null;
+    }
+
+    const token = credential.accessToken;
+    const { user } = result;
+
+    // 액세스 토큰과 유저 정보 반환
+    return { token, user };
+  } catch (error) { // 에러 발생시
+    // eslint-disable-next-line
+    alert(error);
+    return { token: null, user: null }; // {null, null} 리턴
+  }
+};
+
+// 구글 로그아웃
+export const googleLogout = () => {
+  const auth = getAuth();
+  signOut(auth);
+};
