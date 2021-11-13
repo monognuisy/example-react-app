281f7325d67fd57c6cb87f6ff8e460291a1a2c61
monognuisy <ysmsmart1@gmail.com>
2021-11-13 11:41:20 +0900

    fix: fixing type error

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,8 @@
 /* eslint no-bitwise: ["error", { "allow": ["~"] }] */
 const formatTime = (sec: bigint):string => {
-  const hourVal = sec / 3600n;
-  const minuteVal = (sec % 3600n) / 60n;
-  const secondVal = sec % 60n;
+  const hourVal: bigint = sec / 3600n;
+  const minuteVal: bigint = (sec % 3600n) / 60n;
+  const secondVal: bigint = sec % 60n;
 
   let resultString = (hourVal ? `${hourVal.toString()}h ` : '')
   + (minuteVal ? `${minuteVal.toString()}m ` : '');
